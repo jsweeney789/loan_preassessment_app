9343cb0807315db246a12562671c2192caad2f5c
Minh Pham <minhs2@yahoo.com>
2026-05-27 08:44:36 -0700

    Set up data encoding and train/test split for model training

--- backendLoanAssessment/schemas/LoanApplication.py
+++ backendLoanAssessment/schemas/LoanApplication.py
@@ -18,10 +18,10 @@ class EmploymentStatus(str, Enum):
     veryHighlyEmployed = "management / self-employed / highly qualified employee / officer"
 
     employmentNumericMap = {
-        "unemployed":           1,                                             # A171
-        "unskilled":            2,                                             # A172
-        "skilled employee / official": 3,                                      # A173
-        "management / self-employed / highly qualified employee / officer": 4  # A174
+        "unemployed":           0,                                             # A171
+        "unskilled":            1,                                             # A172
+        "skilled employee / official": 2,                                      # A173
+        "management / self-employed / highly qualified employee / officer": 3  # A174
     }
 # the original dataset has resident/non-resident of germany, removed as this would run into fair lending law issues in the US and still maps to a reasonable understanding of a person maybe
 
@@ -34,7 +34,7 @@ class HomeOwnership(str, Enum):
     homeOwnershipNumericMap = {
         "rent": 1,  # A151
         "own": 2,   # A152
-        "free": 3   # A153
+        "free": 0   # A153
     }
 
 class Sex(str, Enum):
@@ -56,7 +56,7 @@ class CheckingAccountStatus(str, Enum):
         "negative_balance": 1,      # A11
         "less_than_250": 2,         # A12
         "250_to_2000": 3,           # A13
-        "no_checking_account": 4    # A14
+        "no_checking_account": 0    # A14
     }
 
 
@@ -72,7 +72,7 @@ class SavingsAccountStatus(str, Enum):
         "500_to_2500": 2,          # A62
         "2500_to_10k": 3,          # A63
         "over_10k": 4,             # A64
-        "no_savings_account": 5,   # A65
+        "no_savings_account": 0,   # A65
     }
 
 
