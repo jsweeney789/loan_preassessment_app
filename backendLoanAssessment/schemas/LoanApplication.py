c76a46a0787c7d385117dce3356ca2617a89ed4e
Alejandro Sanroman <77080173+asanroNA@users.noreply.github.com>
2026-05-29 14:41:13 -0500

    fixing ecs issue, edited environment.ts and dockerfile

--- backendLoanAssessment/schemas/LoanApplication.py
+++ backendLoanAssessment/schemas/LoanApplication.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from pydantic import BaseModel, Field, ConfigDict, field_validator
 from enum import Enum
 
--- frontend-loan-assessment/src/environments/environment.ts
+++ frontend-loan-assessment/src/environments/environment.ts
@@ -11,5 +11,5 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'dev-alb-833559820.us-east-1.elb.amazonaws.com'
+  apiUrl: 'BACKEND_API_URL_PLACEHOLDER'
 };
