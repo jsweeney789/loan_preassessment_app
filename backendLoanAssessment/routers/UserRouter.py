d9eb3acaf81bc5bba2e3931e688cdb9530544821
Jacob <jsweeney@skillstorm.com>
2026-06-04 17:05:49 -0500

    Want to retrigger backend pipeline so that dependencies get rebuilt

--- backendLoanAssessment/routers/UserRouter.py
+++ backendLoanAssessment/routers/UserRouter.py
@@ -8,11 +8,3 @@ router = APIRouter(prefix="/users", tags=["users"])
 def me(user=Depends(getCurrentUser)):
     return user
 
-'''
-Some security notes here:
-
-SpringBoot architecture has a lovely security context system that we get to work within, FastAPI just reuses dependency injection.
-
-For an endpoint that relies on security, we would include user=Depends(getCurrentUser) just like this one, which calls the getCurrentUser and authenticates
-it for us. We can also do this a the touer level.
-'''
